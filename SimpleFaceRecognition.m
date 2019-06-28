ae62ba261ff286945d8de28c72302d7178c83ad8
Yash0330 <yashwanthm0330@gmail.com>
2019-06-28 21:36:44 +0530

    Added Files

--- SimpleFaceRecognition.m
+++ SimpleFaceRecognition.m
@@ -1,25 +1,49 @@
-ds1 = imageDatastore('C:\Users\yash\Documents\photos\SimpleFaceRecognition*.jpg');
-for i = 1:929
-   i1 = readimage(ds1,i);
-   [img,face] = cropface(i1);
-   if face==1
-       imwrite(img,[ 'C:\Users\yash\Documents\croppedfaces',int2str(j), '.jpg']);
-       j = j+1;
-   end
+% n is the number of subjects
+n = 3;
+% looping through all subjects and cropping faces if found
+% extract the subject photo and crop faces and saving it in to respective
+% folders 
+ds1 = imageDatastore('...\s01\img*.jpg');
+for i = 1:size(ds1)
+    i1 = readimage(ds1,i);
+    [img,face] = cropface(i1);
+    if face==1
+        imwrite(img,[ '...\croppedfaces\s01\img',int2str(i), '.jpg']);
+    end
 end
- ds = imageDatastore('C:\Users\yash\Documents\photos\SimpleFaceRecognition\croppedfaces*.jpg');
+ds2 = imageDatastore('...\s02\img*.jpg');
+for i = 1:size(ds2)
+    i1 = readimage(ds2,i);
+    [img,face] = cropface(i1);
+    if face==1
+        imwrite(img,[ '...\croppedfaces\s02\img',int2str(i), '.jpg']);
+    end
+end
+ds3 = imageDatastore('...\s03\img*.jpg');
+for i = 1:size(ds3)
+    i1 = readimage(ds3,i);
+    [img,face] = cropface(i1);
+    if face==1
+        imwrite(img,[ '...\croppedfaces\s03\img',int2str(i), '.jpg']);
+    end
+end
  im = imageDatastore('croppedfaces','IncludeSubfolders',true,'LabelSource','foldernames');
  names = im.Labels;
- fc = fullyConnectedLayer(1);
+ % Resize the images to the input size of the net
+ im.ReadFcn = @(loc)imresize(imread(loc),[227,227]);
+ [Train ,Test] = splitEachLabel(im,0.8);
+ fc = fullyConnectedLayer(n);
  net = alexnet;
  ly = net.Layers;
  ly(23) = fc;
  cl = classificationLayer;
  ly(25) = cl; 
- opts = trainingOptions("sgdm","InitialLearnRate",0.0001,'Plots','training-progress');
- im.ReadFcn = @(loc)imresize(imread(loc),[227,227]);
- [newnet,info] = trainNetwork(im, ly, opts);
- [predict,scores] = classify(newnet,testimg);
+ % options for training the net if your newnet performance is low decrease
+ % the learning_rate
+ learning_rate = 0.0001;
+ opts = trainingOptions("sgdm","InitialLearnRate",learning_rate,'Plots','training-progress');
+ [newnet,info] = trainNetwork(Train, ly, opts);
+ [predict,scores] = classify(newnet,Test);
   
   
   
