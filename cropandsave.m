afa598307c8b73399184d2b4dad520f4fde182f8
Yash0330 <45749966+Yash0330@users.noreply.github.com>
2019-07-03 18:27:50 +0530

    Update cropandsave.m

--- cropandsave.m
+++ cropandsave.m
@@ -1,10 +1,12 @@
-function  cropandsave(im,str,n)
+function  cropandsave(im,str)
 j = 1;
+T = countEachLabel(im);
+n = T(1,2).Variables;
 for i = 1:n
     i1 = readimage(im,i);
     [img,face] = cropface(i1);
     if face==1
-        imwrite(img,[ 'croppedfaces\',str,'\',int2str(j), '.jpg']);
+        imwrite(img,['croppedfaces\', str,'\',int2str(j), '.jpg']);
         j = j+1;
     end
 end
